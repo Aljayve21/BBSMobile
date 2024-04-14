f2bd96ac5eb1c4f9c65fc98b9b465278d83088ef
Aljayve21 <aljayve.capara256@gmail.com>
2024-04-14 23:08:31 +0800

    Adding NavigationBar

--- app/src/main/java/com/example/bbsmobile/SearchFragment.java
+++ app/src/main/java/com/example/bbsmobile/SearchFragment.java
@@ -2,14 +2,21 @@ package com.example.bbsmobile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 
 public class SearchFragment extends Fragment {
+
+
 
 
 
@@ -18,4 +25,6 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+
 }
